dac497402c3140504a82b913e63c836497d2a1df
joonleesky <joonleesky90@gmail.com>
2020-06-20 17:55:27 +0900

    update readme

---
+++ README.md
@@ -0,0 +1,29 @@
+CS231n Convolutional Neural Networks for Visual Recognition - Assignment Solutions
+===============
+
+The course website: http://cs231n.stanford.edu/
+
+My own solutions for CS231N -2018
+
+Assignment list:
+
+ * Assignment #1
+ 	* Q1: k-Nearest Neighbor classifier (20 points) [-done-]
+ 	* Q2: Training a Support Vector Machine (25 points) [-done-]
+ 	* Q3: Implement a Softmax classifier (20 points) [-done-]
+ 	* Q4: Two-Layer Neural Network (25 points) [-done-]
+ 	* Q5: Higher Level Representations: Image Features (10 points) [-done-]
+
+ * Assignment #2
+ 	* Q1: Fully-connected Neural Network (25 points) [-done-]
+ 	* Q2: Batch Normalization (25 points) [-done-]
+ 	* Q3: Dropout (10 points) [-done-]
+ 	* Q4: Convolutional Networks (30 points) [-done-]
+	* Q5: PyTorch / Tensorflow on CIFAR-10 (10 points)[-done-]
+  
+ * Assignment #3
+ 	* Q1: Image Captioning with Vanilla RNNs (25 points)[-done-]
+ 	* Q2: Image Captioning with LSTMs (30 points)[-done-]
+ 	* Q3: Network Visualization: Saliency maps, Class Visualization, and Fooling Images (15 points) [-done-]
+ 	* Q4: Style Transfer (15 points)[-done-]
+	* Q5: Generative Adversarial Networks (15 points)[-done-]
---
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -0,0 +1,114 @@
+import numpy as np
+from random import shuffle
+from past.builtins import xrange
+
+def svm_loss_naive(W, X, y, reg):
+  """
+  Structured SVM loss function, naive implementation (with loops).
+
+  Inputs have dimension D, there are C classes, and we operate on minibatches
+  of N examples.
+
+  Inputs:
+  - W: A numpy array of shape (D, C) containing weights.
+  - X: A numpy array of shape (N, D) containing a minibatch of data.
+  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+    that X[i] has label c, where 0 <= c < C.
+  - reg: (float) regularization strength
+
+  Returns a tuple of:
+  - loss as single float
+  - gradient with respect to weights W; an array of same shape as W
+  """
+  dW = np.zeros(W.shape) # initialize the gradient as zero
+
+  # compute the loss and the gradient
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
+  loss = 0.0
+  for i in xrange(num_train):
+    scores = X[i].dot(W)
+    correct_class_score = scores[y[i]]
+    for j in xrange(num_classes):
+      if j == y[i]:
+        continue
+      margin = scores[j] - correct_class_score + 1 # note delta = 1
+      if margin > 0:
+        dW[:,j] += X[i]
+        dW[:,y[i]] -= X[i]
+        loss += margin
+
+  # Right now the loss is a sum over all training examples, but we want it
+  # to be an average instead so we divide by num_train.
+  loss /= num_train
+  dW /= num_train
+
+  # Add regularization to the loss.
+  loss += 0.5 * reg * np.sum(W * W)
+  dW += reg * W
+
+  #############################################################################
+  # TODO:                                                                     #
+  # Compute the gradient of the loss function and store it dW.                #
+  # Rather that first computing the loss and then computing the derivative,   #
+  # it may be simpler to compute the derivative at the same time that the     #
+  # loss is being computed. As a result you may need to modify some of the    #
+  # code above to compute the gradient.                                       #
+  #############################################################################
+
+
+  return loss, dW
+
+
+def svm_loss_vectorized(W, X, y, reg):
+  """
+  Structured SVM loss function, vectorized implementation.
+
+  Inputs and outputs are the same as svm_loss_naive.
+  """
+  loss = 0.0
+  dW = np.zeros(W.shape) # initialize the gradient as zero
+
+  N = X.shape[0]
+  #############################################################################
+  # TODO:                                                                     #
+  # Implement a vectorized version of the structured SVM loss, storing the    #
+  # result in loss.                                                           #
+  #############################################################################
+  scores = np.dot(X,W)
+  correct_class = scores[np.arange(N),y].reshape(N,1)  
+  scores -= correct_class            #Sj - Sy
+  scores = np.maximum(scores + 1, 0) #max(Sj - Sy + 1, 0)
+  scores[np.arange(N), y] = 0        #if y[i] == j -> 0
+  
+  loss = np.sum(scores)
+
+  loss /= N
+  loss += 0.5 * reg * np.sum(W * W)   #regularization
+  
+  #############################################################################
+  #                             END OF YOUR CODE                              #
+  #############################################################################
+
+
+  #############################################################################
+  # TODO:                                                                     #
+  # Implement a vectorized version of the gradient for the structured SVM     #
+  # loss, storing the result in dW.                                           #
+  #                                                                           #
+  # Hint: Instead of computing the gradient from scratch, it may be easier    #
+  # to reuse some of the intermediate values that you used to compute the     #
+  # loss.                                                                     #
+  #############################################################################
+  dscores = np.zeros_like(scores)
+  dscores = np.where(scores > 0, 1, 0)
+  dscores[np.arange(N), y] -= np.sum(dscores,1)
+  dW = np.dot(X.T, dscores)
+  dW /= N
+  
+  dW += reg * W
+  #############################################################################
+  #                             END OF YOUR CODE                              #
+  #############################################################################
+
+  return loss, dW
